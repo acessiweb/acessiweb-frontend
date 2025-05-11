81f99b6fbede8c8ed0faaf707338fc8688354283
lauravivan <lauvivang@gmail.com>
2025-05-11 19:04:40 -0300

    Update header for visitor, common user and admin

--- src/components/header-mobile.tsx
+++ src/components/header-mobile.tsx
@@ -1,67 +1,110 @@
 import { isAdmin, isCommonUser, isVisitor } from "@/common/utils/authorization";
 import { useSession } from "@/context/auth";
-import { VscHome, VscTerminal, VscListUnordered } from "react-icons/vsc";
 import { usePathname } from "next/navigation";
+import { IoPersonOutline, IoHelp } from "react-icons/io5";
 import {
-  IoCartOutline,
-  IoPersonOutline,
-  IoHelp,
-  IoPersonAddOutline,
-  IoLogInOutline,
-} from "react-icons/io5";
+  SlHome,
+  SlBasket,
+  SlLogin,
+  SlSettings,
+  SlLogout,
+} from "react-icons/sl";
+import { PiPersonArmsSpreadLight } from "react-icons/pi";
 import { useCart } from "@/context/cart";
+import { useState } from "react";
 
 export default function HeaderMobile() {
   const pathname = usePathname();
   const { accessType } = useSession();
   const { guidelinesTotal } = useCart();
+  const [showUserOptions, setShowUserOptions] = useState(false);
 
-  if (!pathname.includes("admin") && !isAdmin(accessType)) {
+  if (pathname.includes("admin") && isAdmin(accessType)) {
     return (
       <header className="header-mobile">
         <div>
-          <div>
-            <VscHome />
-          </div>
-          {isCommonUser(accessType) && (
-            <>
-              <div>
-                <VscTerminal />
-              </div>
-              <div>
-                <IoHelp />
-              </div>
-              <div>
-                <VscListUnordered />
-              </div>
-            </>
-          )}
-          <div style={{ position: "relative" }}>
-            <IoCartOutline />
-            <span className="header__cart-total">{guidelinesTotal}</span>
-          </div>
-          {isVisitor(accessType) && (
-            <>
-              <div>
-                <IoHelp />
-              </div>
-              <div>
-                <IoLogInOutline />
-              </div>
-              <div>
-                <IoPersonAddOutline />
-              </div>
-            </>
-          )}
-          {isCommonUser(accessType) && (
-            <div>
-              <IoPersonOutline />
-            </div>
-          )}
+          <button>
+            <SlHome />
+          </button>
+          <button className="add-guideline">
+            <PiPersonArmsSpreadLight />
+            <span>&#43;</span>
+          </button>
+          <button>
+            <IoHelp />
+          </button>
+          <button>
+            <SlSettings />
+          </button>
+          <button>
+            <SlLogout />
+          </button>
         </div>
       </header>
     );
   }
 
-  return <header></header>;
+  if (!pathname.includes("admin") && isCommonUser(accessType)) {
+    return (
+      <header className="header-mobile">
+        <div>
+          <button>
+            <SlHome />
+          </button>
+          <button className="cart-count">
+            <SlBasket />
+            <span>{guidelinesTotal}</span>
+          </button>
+          <button>
+            <IoHelp />
+          </button>
+          <button>
+            <SlSettings />
+          </button>
+          <button>
+            <SlLogout />
+          </button>
+        </div>
+      </header>
+    );
+  }
+
+  if (!pathname.includes("admin") && isVisitor(accessType)) {
+    return (
+      <header className="header-mobile">
+        <div>
+          <button>
+            <SlHome />
+          </button>
+          <button className="cart-count">
+            <SlBasket />
+            <span>{guidelinesTotal}</span>
+          </button>
+          <button>
+            <IoHelp />
+          </button>
+          <button
+            style={{ position: "relative" }}
+            onClick={() => setShowUserOptions((prev) => !prev)}
+          >
+            <IoPersonOutline />
+            {showUserOptions && (
+              <div className="icon-modal">
+                <button>
+                  <SlLogin />
+                </button>
+                <button className="add-account">
+                  <IoPersonOutline />
+                  <span>&#43;</span>
+                </button>
+              </div>
+            )}
+          </button>
+          <button>
+            <SlSettings />
+          </button>
+        </div>
+      </header>
+    );
+  }
 }
