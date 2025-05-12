baeb197690921dd87e428ef2d532e751c78f465a
lauravivan <lauvivang@gmail.com>
2025-05-12 19:10:40 -0300

    Remove overlay context

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import Header from "@/components/header";
+import HeaderDesktop from "@/components/header-desktop";
 import HeaderMobile from "@/components/header-mobile";
 import Push from "@/components/push";
 import AuthProvider from "@/context/auth";
 import CartProvider from "@/context/cart";
-import { useOverlay } from "@/context/overlay";
 import ProjectProvider from "@/context/projects";
 import { usePush } from "@/context/push";
 import SecPageProvider from "@/context/sec-page";
@@ -14,7 +13,6 @@ import { useEffect, useState } from "react";
 export default function App({ children }: { children?: React.ReactNode }) {
   const { showPush } = usePush();
   const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
-  const { active } = useOverlay();
 
   useEffect(() => {
     setScreenSize({
@@ -31,10 +29,9 @@ export default function App({ children }: { children?: React.ReactNode }) {
         <CartProvider>
           <ProjectProvider>
             <SecPageProvider>
-              {screenSize.width <= 499 ? <HeaderMobile /> : <Header />}
+              {screenSize.width <= 499 ? <HeaderMobile /> : <HeaderDesktop />}
               <main>{children}</main>
               {showPush && <Push />}
-              {active && <div id="overlay"></div>}
             </SecPageProvider>
           </ProjectProvider>
         </CartProvider>
