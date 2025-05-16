0eb66772c54c6fd102cb64b55ef7255537b01cf4
lauravivan <lauvivang@gmail.com>
2025-05-16 18:15:06 -0300

    Delete folder header

--- src/common/header/logout.tsx
+++
@@ -1,9 +0,0 @@
-import { SlLogout } from "react-icons/sl";
-
-export default function Logout() {
-  return (
-    <button className="header__logout" title="Deslogar" aria-label="Deslogar">
-      <SlLogout aria-hidden={true} focusable={false} />
-    </button>
-  );
-}
