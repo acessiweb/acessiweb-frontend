cf4d615c87e5176c3ce80a47ebf70ce60710e3f8
lauravivan <lauvivang@gmail.com>
2025-05-16 18:41:53 -0300

    Add control bar and filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import GuidelinesFilters from "@/common/filters/guidelines";
+import { MOBILE_SCREEN_SIZE } from "@/common/utils/var";
 import CardList from "@/components/card-list";
+import ControlBarMobile from "@/components/control-bar-mobile";
 import Head from "@/components/head";
-import { Search } from "@/components/search";
 import { useCart } from "@/context/cart";
 import { guidelinesStore } from "@/data/guidelines";
+import useScreenSize from "@/hooks/useScreenSize";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [guidelines, setGuidelines] = useState(guidelinesStore);
   const { addGuidelinesToCart } = useCart();
+  const { screenSize } = useScreenSize();
 
   useEffect(() => {
     setGuidelines(guidelinesStore);
@@ -19,67 +23,13 @@ export default function Home() {
     <>
       <Head title="Página inicial - diretrizes de acessibilidade" />
       <div className="homepage">
-        <div>
-          <h1 className="heading-1">Diretrizes de acessibilidade</h1>
-        </div>
-        <form>
-          <div className="homepage__search-guideline">
-            <p>Buscar por</p>
-            <Search searchText="Busque por uma palavra-chave" />
-          </div>
-          <div className="homepage__guideline-checkbox-group">
-            <div>
-              <input
-                className="input-checkbox"
-                type="checkbox"
-                id="visual"
-                name="visual"
-                value="Visual"
-              />
-              <label htmlFor="visual"> Visual</label>
-            </div>
-            <div>
-              <input
-                className="input-checkbox"
-                type="checkbox"
-                id="hearing"
-                name="hearing"
-                value="Auditiva"
-              />
-              <label htmlFor="hearing"> Auditiva</label>
-            </div>
-            <div>
-              <input
-                className="input-checkbox"
-                type="checkbox"
-                id="motor"
-                name="motor"
-                value="Motora"
-              />
-              <label htmlFor="motor"> Motora</label>
-            </div>
-            <div>
-              <input
-                className="input-checkbox"
-                type="checkbox"
-                id="neural"
-                name="neural"
-                value="Neural"
-              />
-              <label htmlFor="neural"> Neural</label>
-            </div>
-            <div>
-              <input
-                className="input-checkbox"
-                type="checkbox"
-                id="tea"
-                name="tea"
-                value="TEA"
-              />
-              <label htmlFor="tea"> TEA</label>
-            </div>
-          </div>
-        </form>
+        <h1 className="heading-1">Diretrizes de acessibilidade</h1>
+        {screenSize.width <= MOBILE_SCREEN_SIZE ? (
+          <ControlBarMobile />
+        ) : (
+          <GuidelinesFilters />
+        )}
+
         <CardList
           data={guidelines}
           hasAdd={true}
