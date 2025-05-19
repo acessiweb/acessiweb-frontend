98a853a2c4cc4da0230c9834968c240d6ccb24d0
lauravivan <lauvivang@gmail.com>
2025-05-19 17:46:24 -0300

    Update cards

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,32 +1,29 @@
 "use client";
 
-import { TABLET_SCREEN_SIZE } from "@/common/utils/var";
-import Card from "@/components/card";
-import useScreenSize from "@/hooks/useScreenSize";
 import { PiPersonArmsSpreadLight } from "react-icons/pi";
 import { SlEnvolope } from "react-icons/sl";
 import GuidelinesAdmin from "./diretrizes/page";
+import { isTablet } from "@/common/utils/size";
+import { CardLink } from "@/components/card-link";
 
 function HomeMobile() {
   return (
     <div className="homepage-mobile">
-      <Card
+      <CardLink
         mainText="Diretrizes de acessibilidade"
         readRoute="/admin/diretrizes"
       >
         <PiPersonArmsSpreadLight aria-hidden={true} focusable={false} />
-      </Card>
-      <Card mainText="Solicitações" readRoute="/admin/solicitacoes">
+      </CardLink>
+      <CardLink mainText="Solicitações" readRoute="/admin/solicitacoes">
         <SlEnvolope aria-hidden={true} focusable={false} />
-      </Card>
+      </CardLink>
     </div>
   );
 }
 
 export default function Home() {
-  const { screenSize } = useScreenSize();
-
-  if (screenSize.width <= TABLET_SCREEN_SIZE) {
+  if (isTablet()) {
     return <HomeMobile />;
   }
 
