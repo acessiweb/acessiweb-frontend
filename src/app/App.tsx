7dc1f358ca41e4a99f010d44ee368632e26ec3d4
lauravivan <lauvivang@gmail.com>
2025-05-19 17:45:42 -0300

    Remove second page provider and add screen size functions checkers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,15 @@
 "use client";
 
-import { TABLET_SCREEN_SIZE } from "@/common/utils/var";
+import { isTablet } from "@/common/utils/size";
 import FooterMobile from "@/components/footer-mobile";
 import HeaderDesktop from "@/components/header-desktop";
 import HeaderMobile from "@/components/header-mobile";
-import Push from "@/components/push";
 import AuthProvider from "@/context/auth";
 import CartProvider from "@/context/cart";
 import ProjectProvider from "@/context/projects";
-import { usePush } from "@/context/push";
-import SecPageProvider from "@/context/sec-page";
-import useScreenSize from "@/hooks/useScreenSize";
 import { useEffect } from "react";
 
 export default function App({ children }: { children?: React.ReactNode }) {
-  const { showPush } = usePush();
-  const { screenSize } = useScreenSize();
-
   useEffect(() => {
     document.body.classList.add("open-sans");
   }, []);
@@ -26,18 +19,11 @@ export default function App({ children }: { children?: React.ReactNode }) {
       <AuthProvider>
         <CartProvider>
           <ProjectProvider>
-            <SecPageProvider>
-              <div className="content">
-                {screenSize.width <= TABLET_SCREEN_SIZE ? (
-                  <HeaderMobile />
-                ) : (
-                  <HeaderDesktop />
-                )}
-                <main>{children}</main>
-              </div>
-              {screenSize.width <= TABLET_SCREEN_SIZE && <FooterMobile />}
-              {showPush && <Push />}
-            </SecPageProvider>
+            <div className="content">
+              {isTablet() ? <HeaderMobile /> : <HeaderDesktop />}
+              <main>{children}</main>
+            </div>
+            {isTablet() && <FooterMobile />}
           </ProjectProvider>
         </CartProvider>
       </AuthProvider>
