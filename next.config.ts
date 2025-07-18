f5dcbc1c17aa82c6fe8203d4410403642c6fe6e4
lauravivan <lauvivang@gmail.com>
2025-07-18 18:24:31 -0300

    Add webpack config to testing cursor images in prod

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/media/[name].[hash][ext]",
+      },
+    });
+
+    return config;
+  },
   devIndicators: false,
 };
 
